9f4bda1639d3d87d872c727241b88c517a0fda01
Logan Negley <logannegley@gmail.com>
2020-02-21 14:44:53 -0600

    no changes

--- src/ContactForm.test.js
+++ src/ContactForm.test.js
@@ -8,10 +8,11 @@ test('renders First Name,Last Name, Email and Message inputs', ()=>{
 //Arrange
 const {getByText} = render(<ContactForm/>);
 // Act
-getByText(/first name/i);
-getByText(/Last Name/i);
-getByText(/Email/i);
-getByText(/Message/i);
+const firstNameInput = getByText(/First name/i);
+const lastNameInput = getByText(/Last Name/i);
+const emailInput = getByText(/Email/i);
+const messageInput = getByText(/Message/i);
 //Assert
+expect(messageInput).toBeInTheDocument()
+});
 
-})
